71e5285a6141862a58be809b99bc767806079b1a
dleess <92695827+dleess@users.noreply.github.com>
2021-12-04 12:18:53 -0500

    Add files via upload

---
+++ hello_mc_ml/src/sine_model_data.h
@@ -0,0 +1,12 @@
+// This is a standard TensorFlow Lite model file that has been converted into a
+// C data array, so it can be easily compiled into a binary for devices that
+// don't have a file system. It was created using the command:
+// xxd -i sine_model.tflite > sine_model_data.cc
+
+#ifndef TENSORFLOW_LITE_EXPERIMENTAL_MICRO_EXAMPLES_HELLO_WORLD_SINE_MODEL_DATA_H_
+#define TENSORFLOW_LITE_EXPERIMENTAL_MICRO_EXAMPLES_HELLO_WORLD_SINE_MODEL_DATA_H_
+
+extern const unsigned char g_sine_model_data[];
+extern const int g_sine_model_data_len;
+
+#endif  // TENSORFLOW_LITE_EXPERIMENTAL_MICRO_EXAMPLES_HELLO_WORLD_SINE_MODEL_DATA_H_
